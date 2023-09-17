680d96ba4166a982ad17e5f4266576ee19ae0fe2
mmontesl1 <mmontesl1@eafit.edu.co>
2023-09-17 15:14:53 -0500

    se agregan rutas de add y editar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//componentes
+import { ListPersonalComponent } from './components/list-personal/list-personal.component';
+import { AddEditPersonalComponent } from './components/add-edit-personal/add-edit-personal.component';
+
+const routes: Routes = [
+  //ruta de personal
+  {path: '', component: ListPersonalComponent},
+  //ruta de add y de editar los cuales son el mismo componente
+  {path: 'add', component: AddEditPersonalComponent},
+  {path: 'edit/:id', component: AddEditPersonalComponent},
+  //si se escribe una url que no exista redirecciona a personal
+  {path: '**', redirectTo: '', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/list-personal/list-personal.component.ts
+++ src/app/components/list-personal/list-personal.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Personal } from 'src/app/interfaces/personal';
 
 @Component({
   selector: 'app-list-personal',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-personal.component.css']
 })
 export class ListPersonalComponent {
-
+  listPersonal: Personal[] = [
+    {id:1, documento: '1234556' , primernombre: 'Mateo' , primerapellido: 'Montes' , telefono: '3225272929' , sexo: 'Masculico' , correo: ' mateomontes@gmail.es', foto: "url"},
+    {id:2, documento: '1234557' , primernombre: 'Marta' , primerapellido: 'Montes' , telefono: '3225272928' , sexo: 'Femenino' , correo: ' martamontes@gmail.es', foto: "url"},
+  ]
 }
